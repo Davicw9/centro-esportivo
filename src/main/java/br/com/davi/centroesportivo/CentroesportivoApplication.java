73f800f64753e923c94274a9c2eaf27b63e681e8
Davicw9 <davi.andrade.2023@hotmail.com>
2026-02-23 06:03:43 -0300

    cria o projeto com as dependencias

---
+++ src/main/java/br/com/davi/centroesportivo/CentroesportivoApplication.java
@@ -0,0 +1,13 @@
+package br.com.davi.centroesportivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroesportivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentroesportivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/davi/centroesportivo/CentroesportivoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.davi.centroesportivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroesportivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
